f08a5e5d65ae12e869765b954cd29bcfd4f8fc93
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2018-02-18 21:15:48 +0200

    Write a POC

--- src/runReporters.ts
+++ src/runReporters.ts
@@ -4,9 +4,25 @@
 //
 // Not to be confused with https://www.npmjs.com/package/aws-lambda
 import { Handler } from 'aws-lambda'; // tslint:disable-line:no-implicit-dependencies
+import WebPageTest from 'webpagetest';
+import bluebird from 'bluebird';
 
-const runReporters: Handler = (_event, _context, callback) => {
+const API_KEY = process.env.WPT_API_KEY;
+
+export const runReporters: Handler = async (_event, _context, callback) => {
+  const wpt = new WebPageTest('www.webpagetest.org', API_KEY);
+
+  const runTest = bluebird.promisify(wpt.runTest).bind(wpt);
+  const getTestResults = bluebird.promisify(wpt.getTestResults).bind(wpt);
+  const { data: { testId } }: WebPageTest.RunTestResponse = await runTest(
+    'https://hollowverse.com',
+    {
+      lighthouse: true,
+    },
+  );
+
+  // tslint:disable-next-line:no-console
+  console.log(await getTestResults(testId));
+
   callback(null, 'Hello World');
 };
-
-exports.runReporters = runReporters;
