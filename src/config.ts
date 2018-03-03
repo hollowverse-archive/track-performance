b4c137ed29c20358733a6c622731fb98b831e75f
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2018-03-04 00:01:42 +0200

    Remove unneeded public key

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,8 @@
 import { join } from 'path';
 
-const sshKeyPublicKeyPath = join(process.cwd(), 'secrets/sshPublicKey');
 const sshPrivateKeyPath = join(process.cwd(), 'secrets/sshPrivateKey');
 
 export const config = {
-  sshKeyPublicKeyPath,
   sshPrivateKeyPath,
   webpagetest: {
     apiKey: process.env.WPT_API_KEY,
