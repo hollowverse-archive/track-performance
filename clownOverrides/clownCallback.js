84cf9adf3bc043b05061cd5e2207e280cec51b49
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2018-04-16 22:20:41 +0300

    Update clown and config

---
+++ clownOverrides/clownCallback.js
@@ -0,0 +1,8 @@
+/* eslint-disable import/no-extraneous-dependencies */
+const { omit } = require('lodash');
+
+module.exports = clownFs => {
+  clownFs.editJson('package.json', json =>
+    omit(json, 'devDependencies.prettier'),
+  );
+};
