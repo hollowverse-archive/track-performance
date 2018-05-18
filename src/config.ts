deace21290faf8432822e4b54d658b997787df69
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2018-05-18 20:50:11 +0300

    Remove git and github logic, secrets and packages

--- src/config.ts
+++ src/config.ts
@@ -3,20 +3,13 @@ import { readAwsSecretsForStage } from '@hollowverse/utils/helpers/readAwsSecret
 export const getConfig = async () => {
   const secrets = await readAwsSecretsForStage([
     'wpt/apiKey',
-    'github/hollowbot/accessToken',
     'google/searchConsoleApis/apiKey',
-    'hollowbot/sshPrivateKey',
   ]);
 
   return {
-    sshPrivateKey: secrets['hollowbot/sshPrivateKey'],
     shouldPush: process.env.NODE_ENV === 'production',
-    shouldInstallGit: process.env.NODE_ENV !== 'local',
     webpagetest: {
       apiKey: secrets['wpt/apiKey'],
-    },
-    github: {
-      token: secrets['github/hollowbot/accessToken'],
     },
     google: {
       apiKey: secrets['google/searchConsoleApis/apiKey'],
--- src/reportPerformance.ts
+++ src/reportPerformance.ts
@@ -1,19 +1,10 @@
-import Octokit from '@octokit/rest';
 import bluebird from 'bluebird';
-import initGit from 'lambda-git';
 import prettier from 'prettier';
-import shelljs from 'shelljs';
-import tmp from 'tmp';
 import { collectReports } from './helpers/collectReports';
 import { getConfig } from './config';
 import { format as formatDate } from 'date-fns';
-import { join } from 'path';
 import { renderReport } from './helpers/renderReport';
 import { stripIndents } from 'common-tags';
-import { writeFile } from './helpers/writeFile';
-import { executeCommand } from '@hollowverse/utils/helpers/executeCommand';
-import { executeCommands } from '@hollowverse/utils/helpers/executeCommands';
-import { retryCommand } from '@hollowverse/utils/helpers/retryCommand';
 import { SecurityHeadersReporter } from './reporters/SecurityHeadersReporter';
 import { WebPageTestReporter } from './reporters/WebPageTestReporter';
 import { MobileFriendlinessReporter } from './reporters/MobileFriendlinessReporter';
@@ -24,13 +15,7 @@ import { GenericReporterClass, PageReporterClass } from './typings/reporter';
 export const reportPerformance = async () => {
   const config = await getConfig();
 
-  const urls = [
-    'https://hollowverse.com',
-    'https://hollowverse.com/Tom_Hanks',
-    'https://dev.hollowverse.com',
-    'https://dev.hollowverse.com/Tom_Hanks',
-  ];
-  const dateStr = formatDate(new Date(), 'YYYY-MM-DD');
+  const urls = ['https://hollowverse.com', 'https://hollowverse.com/Tom_Hanks'];
 
   const pageReporters: PageReporterClass[] = [
     SecurityHeadersReporter,
@@ -60,128 +45,35 @@ export const reportPerformance = async () => {
   const genericReports = await genericReportersPromise;
   const pageReports = await pageReportsPromise;
 
-  let markdownReport = stripIndents`
-    Report for tests performed on ${dateStr}
-    ========================================
-
-    ${pageReports
-      .map(
-        ({ url, reports }) => `
-      ## ${url}
-
-      ${reports
-        .map(report => renderReport(report, { headingLevel: 3 }))
-        .join('\n'.repeat(2))}
-    `,
-      )
-      .join('\n'.repeat(2))}
-
-    ${genericReports
-      .map(report => renderReport(report, { headingLevel: 2 }))
-      .join('\n'.repeat(2))}
-  `;
-
-  markdownReport = prettier.format(markdownReport, { parser: 'markdown' });
-
   if (process.env.STAGE === 'local') {
+    const dateStr = formatDate(new Date(), 'YYYY-MM-DD');
+    let markdownReport = stripIndents`
+      Report for tests performed on ${dateStr}
+      ========================================
+
+      ${pageReports
+        .map(
+          ({ url, reports }) => `
+        ## ${url}
+
+        ${reports
+          .map(report => renderReport(report, { headingLevel: 3 }))
+          .join('\n'.repeat(2))}
+      `,
+        )
+        .join('\n'.repeat(2))}
+
+      ${genericReports
+        .map(report => renderReport(report, { headingLevel: 2 }))
+        .join('\n'.repeat(2))}
+    `;
+
+    markdownReport = prettier.format(markdownReport, { parser: 'markdown' });
     console.info(markdownReport);
 
     return;
   }
 
   if (config.shouldPush) {
-    const repoPath = tmp.dirSync().name;
-    const branchName = `report-${dateStr}`;
-    const filesToAdd = {
-      'mostRecent.md': markdownReport,
-    };
-
-    await executeCommands([
-      async () => {
-        if (config.shouldInstallGit) {
-          await initGit();
-          shelljs.env.LD_LIBRARY_PATH += ':/tmp/git/usr/lib64';
-        }
-      },
-      async () => {
-        if (!config.sshPrivateKey) {
-          throw new TypeError(
-            'Expected SSH private key to be provided in configuration',
-          );
-        }
-        await writeFile('/tmp/privateKey', config.sshPrivateKey);
-        shelljs.env.GIT_SSH_COMMAND =
-          'ssh -o StrictHostKeyChecking=no -i /tmp/privateKey';
-      },
-      'chmod 600 /tmp/privateKey',
-      `git clone git@github.com:hollowverse/perf-reports.git ${repoPath}`,
-      () => {
-        shelljs.cd(repoPath);
-      },
-      'git config --local user.name hollowbot',
-      'git config --local user.email hollowbot@hollowverse.com',
-      `git checkout -b ${branchName}`,
-      async () => {
-        await bluebird.map(
-          Object.entries(filesToAdd),
-          async ([fileName, contents]) => {
-            await writeFile(join(repoPath, fileName), contents);
-          },
-        );
-      },
-      `git add ${Object.keys(filesToAdd).join(' ')}`,
-      `git commit -m 'Update report file with results from ${dateStr}'`,
-      `git push origin -u ${branchName} --force`,
-    ]);
-
-    if (config.github.token === undefined) {
-      throw new TypeError(
-        'GitHub access token is required but was not provided',
-      );
-    }
-
-    const octokit = new Octokit();
-
-    octokit.authenticate({
-      token: config.github.token,
-      type: 'token',
-    });
-
-    const { data: { number } } = await octokit.pullRequests.create({
-      owner: 'hollowverse',
-      repo: 'perf-reports',
-      base: 'master',
-      head: branchName,
-      // @ts-ignore
-      title: `Update report to ${dateStr}`,
-      body: markdownReport,
-    });
-
-    try {
-      await octokit.issues.edit({
-        owner: 'hollowverse',
-        repo: 'perf-reports',
-        number,
-        labels: ['report'],
-      });
-    } catch (error) {
-      console.error(`Failed to set label on PR ${number}: ${error.message}`);
-    }
-
-    try {
-      await retryCommand(async () => {
-        await octokit.pullRequests.merge({
-          owner: 'hollowverse',
-          repo: 'perf-reports',
-          number,
-          merge_method: 'squash',
-        });
-      }, 3);
-
-      await executeCommand(`git push origin --delete ${branchName}`);
-    } catch (error) {
-      console.error(`Failed to merge PR ${number}: ${error.message}`);
-    }
-    console.info('Pull request created');
   }
 };
